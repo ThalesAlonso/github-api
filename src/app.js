685229987368ab135664f9b401926dd5a9d406e8
ThalesAlonso <koben_rock@hotmail.com>
2019-12-27 13:21:05 -0200

    config .gitignore

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react'
-import ajax from '@ThalesAlonso/ajax'
+import ajax from '@fdaciuk/ajax'
 import AppContent from './components/app-content'
 
 class App extends Component {
